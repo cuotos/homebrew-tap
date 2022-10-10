ecbbb19f8acb8daf49895c07b91f819b34d918c3
Dan P <dan@danpotepa.co.uk>
2022-10-10 17:11:34 +0100

    Update aws-config-profiles.rb

--- Formula/aws-config-profiles.rb
+++ Formula/aws-config-profiles.rb
@@ -1,7 +1,7 @@
 class StsAssumeRoleEnvVars < Formula
   desc 'List the AWS profiles and account numbers in the aws config file'
   homepage 'https://github.com/cuotos/aws-config-profiles'
-  version '1.0.1'
+  version '0.0.0'
   url "cxxx"
   sha256 'xxx'
 
