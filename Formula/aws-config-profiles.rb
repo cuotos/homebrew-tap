9756331362e8f8fa644cd2e405eca242fb413cda
Dan Potepa <dan@danpotepa.co.uk>
2022-10-10 17:05:16 +0100

    added aws-config-profiles

---
+++ Formula/aws-config-profiles.rb
@@ -0,0 +1,11 @@
+class StsAssumeRoleEnvVars < Formula
+  desc 'List the AWS profiles and account numbers in the aws config file'
+  homepage 'https://github.com/cuotos/aws-config-profiles'
+  version '1.0.1'
+  url "cxxx"
+  sha256 'xxx'
+
+  def install
+    bin.install 'aws-config-profiles'
+  end
+end
