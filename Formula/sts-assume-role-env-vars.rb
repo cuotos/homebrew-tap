32fc269fff73c38516e873fd2522a02aec5d6d6b
Dan P <dan@danpotepa.co.uk>
2022-12-30 18:23:56 +0000

    sts-assume-role-env-vars 1.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/sts-assume-role-env-vars.rb
+++ Formula/sts-assume-role-env-vars.rb
@@ -1,9 +1,9 @@
 class StsAssumeRoleEnvVars < Formula
   desc 'Automatically convert AWS sts assume-role output to env vars'
   homepage 'https://github.com/cuotos/sts-assume-role-env-vars'
-  version '1.0.2'
-  url "https://github.com/cuotos/sts-assume-role-env-vars/releases/download/v1.0.2/sts-assume-role-env-vars_1.0.2_Darwin_x86_64.tar.gz"
-  sha256 '8bd1045f5fad1fb168a921c34f0449f1ea28d7a993b9d5a7d77342f9c95f065e'
+  version '1.0.3'
+  url "https://github.com/cuotos/sts-assume-role-env-vars/releases/download/v1.0.3/sts-assume-role-env-vars_1.0.3_Darwin_x86_64.tar.gz"
+  sha256 '62043ded0886033b458250b95304cad0b635c40bdfc873e871a439de10cc75ff'
 
   def install
     bin.install 'sts-assume-role-env-vars'
