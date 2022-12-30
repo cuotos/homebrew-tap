cbbd463f730675367d7ff10993dc3b4b0c085ddc
Dan P <dan@danpotepa.co.uk>
2022-12-30 18:25:41 +0000

    epoch 0.0.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/epoch.rb
+++ Formula/epoch.rb
@@ -1,9 +1,9 @@
 class Epoch < Formula
     desc 'Convert epoch to human readable date'
     homepage 'https://github.com/cuotos/epoch'
-    version '0.0.1'
-    url "https://github.com/cuotos/epoch/releases/download/v0.0.1/epoch_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 'fc4fd6d46f27d3eb9cdc388b42c0bc0bb704fe8c186877192c4706d8691cf468'
+    version '0.0.2'
+    url "https://github.com/cuotos/epoch/releases/download/v0.0.2/epoch_0.0.2_Darwin_x86_64.tar.gz"
+    sha256 '0a6cbb284908a6a4e98b07501325a475a97a92baebb6e26b9e85c5bf37d801bf'
   
     def install
       bin.install 'epoch'
