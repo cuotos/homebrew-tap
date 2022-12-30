da0a8b83208eee8825717c6b989af2e598183668
Dan P <dan@danpotepa.co.uk>
2022-12-30 17:54:47 +0000

    outstanding-prs 0.3.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/outstanding-prs.rb
+++ Formula/outstanding-prs.rb
@@ -1,9 +1,9 @@
 class OutstandingPrs < Formula
   desc 'List PRs from github team that are waiting for reviews'
   homepage 'https://github.com/cuotos/outstanding-prs'
-  version '0.3.0'
-  url "https://github.com/cuotos/outstanding-prs/releases/download/v#{version}/outstanding-prs_#{version}_Darwin_x86_64.tar.gz"
-  sha256 '0ccf83fc638eaadfeaf72b94eec0c615d4d52c3aba88658b4418e7dedcdc93d5'
+  version '0.3.3'
+  url "https://github.com/cuotos/outstanding-prs/releases/download/v0.3.3/outstanding-prs_0.3.3_Darwin_x86_64.tar.gz"
+  sha256 'cc65129ccaac61308febcca8218757740b10c2f46fc277b039917096243c1ffa'
 
   def install
     bin.install 'outstanding-prs'
