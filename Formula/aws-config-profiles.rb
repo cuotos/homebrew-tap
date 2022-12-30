49cc1af1be393a67555ac7221187bbd8000b635d
Dan P <dan@danpotepa.co.uk>
2022-12-30 18:05:01 +0000

    aws-config-profiles 0.0.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/aws-config-profiles.rb
+++ Formula/aws-config-profiles.rb
@@ -1,9 +1,9 @@
 class AwsConfigProfiles < Formula
   desc 'List the AWS profiles and account numbers in the aws config file'
   homepage 'https://github.com/cuotos/aws-config-profiles'
-  version '0.0.4'
-  url "https://github.com/cuotos/aws-config-profiles/releases/download/v0.0.4/aws-config-profiles_0.0.4_Darwin_x86_64.tar.gz"
-  sha256 'a8faaf03e0180248a6f64fe16617266b2e5845c17465dbb5d2deb2b21ade5d5c'
+  version '0.0.5'
+  url "https://github.com/cuotos/aws-config-profiles/releases/download/v0.0.5/aws-config-profiles_0.0.5_Darwin_x86_64.tar.gz"
+  sha256 'b1c22207df68157b8ea851db1dc19fb91280342e379f02942463a27ba2ba9195'
 
   def install
     bin.install 'aws-config-profiles'
