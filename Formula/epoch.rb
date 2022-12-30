835054b39c5560f62e26ac4835dc4743d5cebaa4
Dan P <dan@danpotepa.co.uk>
2022-12-30 17:08:12 +0000

    epoch 0.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/epoch.rb
+++ Formula/epoch.rb
@@ -1,9 +1,9 @@
 class Epoch < Formula
     desc 'Convert epoch to human readable date'
     homepage 'https://github.com/cuotos/epoch'
-    version '0.0.0'
-    url "xxx"
-    sha256 'xxx'
+    version '0.0.1'
+    url "https://github.com/cuotos/aws-config-profiles/releases/download/v0.0.1/aws-config-profiles_0.0.1_Darwin_x86_64.tar.gz"
+    sha256 '6381a23fb94054438abaa8cf8318c9f39276664ea165a99cda95048ef46c475f'
   
     def install
       bin.install 'epoch'
