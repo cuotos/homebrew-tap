d30f6288d0bf4da827c34335e4efdb0fe885bdcb
Dan Potepa <dan@danpotepa.co.uk>
2022-12-30 17:02:38 +0000

    added epoch

---
+++ Formula/epoch.rb
@@ -0,0 +1,12 @@
+class Epoch < Formula
+    desc 'Convert epoch to human readable date'
+    homepage 'https://github.com/cuotos/epoch'
+    version '0.0.0'
+    url "xxx"
+    sha256 'xxx'
+  
+    def install
+      bin.install 'epoch'
+    end
+  end
+  
