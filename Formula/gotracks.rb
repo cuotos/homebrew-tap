8e52cfe7850a36a7d1fd0ed9388c760e81a6ddb9
Dan P <dan@danpotepa.co.uk>
2023-01-05 13:47:37 +0000

    gotracks 0.0.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/gotracks.rb
+++ Formula/gotracks.rb
@@ -1,9 +1,9 @@
 class Gotracks < Formula
     desc 'Get guitar tabs and lyrics for current playing song'
     homepage 'https://github.com/cuotos/gotracks'
-    version '0.0.1'
-    url "https://github.com/cuotos/gotracks/releases/download/v0.0.1/gotracks_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 'fcaac8b1322e4541a4a44e494234db55a4a331d6e860c140167c43b2cf2aafec'
+    version '0.0.2'
+    url "https://github.com/cuotos/gotracks/releases/download/v0.0.2/gotracks_0.0.2_Darwin_x86_64.tar.gz"
+    sha256 '1a2fd81a40dd5084f9277fe5e3919b2a00586e967ba2b6560dac2495c2f475fc'
   
     def install
       bin.install 'gotracks'
