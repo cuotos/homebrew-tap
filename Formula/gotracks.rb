d35b2789cda1410d3aabc029527246f2ebdc18fd
Dan Potepa <dan@danpotepa.co.uk>
2023-01-05 11:27:20 +0000

    add gotracks

---
+++ Formula/gotracks.rb
@@ -0,0 +1,12 @@
+class Epoch < Formula
+    desc 'Get guitar tabs and lyrics for current playing song'
+    homepage 'https://github.com/cuotos/gotracks'
+    version '0.0.0'
+    url "xxx"
+    sha256 'xxx'
+  
+    def install
+      bin.install 'gotracks'
+    end
+  end
+  
