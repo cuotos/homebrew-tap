e8e8be069af8ded349827b2aa7678d6057ffc73b
Dan P <dan@danpotepa.co.uk>
2023-07-26 17:51:15 +0100

    Update get-local-lambda.rb

--- Formula/get-local-lambda.rb
+++ Formula/get-local-lambda.rb
@@ -2,8 +2,8 @@ class GetLocalLambda < Formula
     desc 'Proxy a get request to a locally running lambda'
     homepage 'https://github.com/cuotos/get-local-lambda'
     version '0.0.0'
-    url "https://github.com/cuotos/epoch/releases/download/v0.0.3/epoch_0.0.3_Darwin_x86_64.tar.gz"
-    sha256 '01f8e4960d30550e19a689a9214438c0cb57e5a3fdfb36bc46fa8ff29e98df92'
+    url "https://github.com/cuotos/epoch/releases/download/v0.0.0/get_local_lambda_0.0.0_Darwin_x86_64.tar.gz"
+    sha256 'xx'
   
     def install
       bin.install 'get-local-lambda'
