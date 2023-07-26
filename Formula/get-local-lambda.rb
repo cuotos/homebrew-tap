ce1b1b55ef13ddf99e21d9fc186ca63413e379f5
Dan P <dan@danpotepa.co.uk>
2023-07-26 17:51:35 +0100

    get-local-lambda 0.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/get-local-lambda.rb
+++ Formula/get-local-lambda.rb
@@ -1,9 +1,9 @@
 class GetLocalLambda < Formula
     desc 'Proxy a get request to a locally running lambda'
     homepage 'https://github.com/cuotos/get-local-lambda'
-    version '0.0.0'
-    url "https://github.com/cuotos/epoch/releases/download/v0.0.0/get_local_lambda_0.0.0_Darwin_x86_64.tar.gz"
-    sha256 'xx'
+    version '0.0.1'
+    url "https://github.com/cuotos/get-local-lambda/releases/download/v0.0.1/get-local-lambda_0.0.1_Darwin_x86_64.tar.gz"
+    sha256 '424495bd3f40036cbc6a89dd2d937feb65bcdfb6b5a6cc3657e676accfe09bd8'
   
     def install
       bin.install 'get-local-lambda'
